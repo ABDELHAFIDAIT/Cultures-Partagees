08beaa13e0728e7584f2b2dab14506b36e5b0e03
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2024-12-31 12:50:30 +0100

    update path to login page in index.php + create the page login

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome Page</title>
+    <title>CultureConnect - Welcome Page</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="./assets/css/style.css">
     <link rel="stylesheet" href="./assets/css/output.css">
@@ -17,7 +17,7 @@
             
             <h1 class="text-5xl font-bold mb-4 animate-bounce">Welcome to Culture <span class="text-blue-400">Connect</span></h1>
             <p class="text-xl mb-8 text-gray-300">Discover and share cultures from around the world.</p>
-            <a href="login.php" class="bg-blue-900 hover:bg-blue-700 text-white font-medium py-2 px-10 rounded-sm transition duration-300 ease-in-out transform hover:scale-105">
+            <a href="./views/login.php" class="bg-blue-900 hover:bg-blue-700 text-white font-medium py-2 px-10 rounded-sm transition duration-300 ease-in-out transform hover:scale-105">
                 Login
             </a>
         </div>
---
+++ views/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/output.css">
+</head>
+<body>
+    <header>
+
+    </header>
+
+    <main>
+
+    </main>
+
+    <?php include_once '../includes/footer.php'; ?>
+</body>
+</html>
