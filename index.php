4d4250feda73b1039066dfd657a4a6dbbee0641a
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2024-12-31 12:45:07 +0100

    Finishing index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/output.css">
+</head>
+<body>
+    <main class="redirection flex items-center justify-center h-screen text-white px-5">
+        <div class="text-center">
+            <div class="flex items-center justify-center">
+                <img src="./assets/img/logo.png" class="w-1/3 mb-10" alt="Logo de CultureConnect">
+            </div>
+            
+            <h1 class="text-5xl font-bold mb-4 animate-bounce">Welcome to Culture <span class="text-blue-400">Connect</span></h1>
+            <p class="text-xl mb-8 text-gray-300">Discover and share cultures from around the world.</p>
+            <a href="login.php" class="bg-blue-900 hover:bg-blue-700 text-white font-medium py-2 px-10 rounded-sm transition duration-300 ease-in-out transform hover:scale-105">
+                Login
+            </a>
+        </div>
+    </main>
+</body>
+</html>
