801619db26eaee5071e3ee97384d8f5dd996fcdb
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2025-01-01 20:52:43 +0100

    Terminer la page Articles et Ajout de navbar et footer dans includes

---
+++ includes/navbar.php
@@ -0,0 +1,11 @@
+<nav class="bg-[#b316bd] px-10 py-5">
+    <div class="container mx-auto flex justify-between items-center">
+        <div class="text-white text-2xl font-bold">
+            Culture<span class="text-black">Connect</span>
+        </div>
+        <div class="flex">
+            <button
+                class="border border-white text-white py-1 px-4 text-sm duration-300 hover:border-none hover:bg-black hover:scale-105">Déconnexion</button>
+        </div>
+    </div>
+</nav>
