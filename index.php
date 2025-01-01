f407869d8aaf4515e89e3b2723edfc0bee2ca76d
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2025-01-01 10:14:10 +0100

    Ajouter la page de login + update bg in home page

--- index.php
+++ index.php
@@ -4,18 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CultureConnect - Welcome Page</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="./assets/css/style.css">
     <link rel="stylesheet" href="./assets/css/output.css">
 </head>
 <body>
     <main class="redirection flex items-center justify-center h-screen text-white px-5">
         <div class="text-center">
-            <div class="flex items-center justify-center">
-                <img src="./assets/img/logo.png" class="w-1/3 mb-10" alt="Logo de CultureConnect">
-            </div>
-            
-            <h1 class="text-5xl font-bold mb-4 animate-bounce">Welcome to Culture <span class="text-blue-400">Connect</span></h1>
+            <h1 class="text-5xl font-bold mb-4 animate-bounce">Welcome to Culture<span class="text-blue-500">Connect</span></h1>
             <p class="text-xl mb-8 text-gray-300">Discover and share cultures from around the world.</p>
             <a href="./views/login.php" class="bg-blue-900 hover:bg-blue-700 text-white font-medium py-2 px-10 rounded-sm transition duration-300 ease-in-out transform hover:scale-105">
                 Login
