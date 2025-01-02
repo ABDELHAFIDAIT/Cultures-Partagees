6ff4f2c1278d5e16a0886150a5acb42eb9b724dd
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2025-01-02 15:02:05 +0100

    Add Show All Articles Method

--- classes/article.php
+++ classes/article.php
@@ -42,4 +42,22 @@
         public function setDate($date){
             $this->date_pub = $date;
         }
+
+
+        // SHOW ALL ARTICLES
+        public function allArticles(){
+            try{
+                $query = "SELECT * FROM articles";
+                $stmt = $this->database->getConnection()->prepare($query);
+                $stmt->execute();
+                if($stmt->rowCount() > 0){
+                    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    return $result;
+                }else{
+                    return false;
+                }
+            }catch(PDOException $e){
+                return "Erreur lors de la Récupération des Articles". $e->getMessage();
+            }
+        }
     }
