3921e57ecbf86cfdb4ce046091b74a9a898f8296
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2025-01-02 13:14:33 +0100

    Ajouter les fonctions de modification et suppression d'un article

--- classes/auteur.php
+++ classes/auteur.php
@@ -16,7 +16,38 @@
                 $stmt->execute();
                 header("location: ../views/admin/dashboard.php");
             } catch (PDOException $e) {
-                return "Erreur lors de l'ajout de la catégorie : " . $e->getMessage();
+                return "Erreur lors de l'ajout de l'Article : " . $e->getMessage();
+            }
+        }
+
+
+        // MODIFY ARTICLE METHOD
+        public function editArticle(int $id_article, string $titre , string $contenu, int $id_categorie){
+            try{
+                $sql = "UPDATE article SET titre = :titre, contenu = :contenu, id_categorie = :id_categorie WHERE id_article = :id_article";
+                $stmt = $this->database->getConnection()->prepare($sql);
+                $stmt->bindParam(":titre", $titre, PDO::PARAM_STR);
+                $stmt->bindParam(":contenu", $contenu, PDO::PARAM_STR);
+                $stmt->bindParam(":id_categorie", $id_categorie, PDO::PARAM_INT);
+                $stmt->bindParam(":id_article", $id_article, PDO::PARAM_INT);
+                $stmt->execute();
+                header("location: ../views/admin/dashboard.php");
+            } catch (PDOException $e) {
+                return "Erreur lors de modification de l'Article : ". $e->getMessage();
+            }
+        }
+        
+
+        // DELETE ARTICLE METHOD
+        public function deleteArticle(int $id_article){
+            try{
+                $sql = "DELETE FROM article WHERE id_article = :id_article";
+                $stmt = $this->database->getConnection()->prepare($sql);
+                $stmt->bindParam(":id_article", $id_article, PDO::PARAM_INT);
+                $stmt->execute();
+                header("location: ../views/admin/dashboard.php");
+            } catch (PDOException $e) {
+                return "Erreur lors de la suppression de l'Article : ". $e->getMessage();
             }
         }
 
