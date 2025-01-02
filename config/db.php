08a854e9ca7f09f60def75b7ea756f05ecc1f435
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2025-01-02 09:31:12 +0100

    Ajouter la classe Database

---
+++ config/db.php
@@ -0,0 +1,40 @@
+<?php
+
+class Database {
+    private $host;
+    private $db;
+    private $user;
+    private $pass;
+    private $charset;
+    private $pdo;
+
+    
+    public function __construct() {
+        $this->host ='localhost';
+        $this->db ='culture_partagee';
+        $this->user = 'root';
+        $this->pass = '';
+        $this->charset ='utf8mb4';
+
+        $this->connect();
+    }
+
+    private function connect() {
+        $dsn = "mysql:host=$this->host;dbname=$this->db;charset=$this->charset";
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+        try {
+            $this->pdo = new PDO($dsn, $this->user, $this->pass, $options);
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+
+    public function getConnection() {
+        return $this->pdo;
+    }
+}
