582998a3154e524b84327a05ae4640597428e7ad
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2025-01-06 21:32:25 +0100

    AJouter la sécurité contre les Failles XSS dans la fonction de Login

--- actions/login.php
+++ actions/login.php
@@ -6,30 +6,29 @@
 
     if($_SERVER['REQUEST_METHOD']==='POST'){
         if(isset($_POST['login'])){
-            $email = trim($_POST['email']);
-            $password = $_POST['password'];
+            $email = htmlspecialchars(trim($_POST['email']), ENT_QUOTES, 'UTF-8');
+            $password = htmlspecialchars($_POST['password'], ENT_QUOTES, 'UTF-8');
 
             if (empty($email) || empty($password)) {
                 echo "Veuillez remplir tous les champs.";
-            }else {
-                
+            } else {
                 $user = new User();
     
-                $loggedInUser = $user->login( $email, $password);
+                $loggedInUser = $user->login($email, $password);
     
                 if ($loggedInUser) {
-                    $_SESSION['id_user'] = $loggedInUser->getId();
-                    $_SESSION['prenom'] = $loggedInUser->getPrenom();
-                    $_SESSION['nom'] = $loggedInUser->getNom();
-                    $_SESSION['email'] = $loggedInUser->getEmail();
-                    $_SESSION['phone'] = $loggedInUser->getTelephone();
-                    $_SESSION['role'] = $loggedInUser->getRole();
+                    $_SESSION['id_user'] = htmlspecialchars($loggedInUser->getId(), ENT_QUOTES, 'UTF-8');
+                    $_SESSION['prenom'] = htmlspecialchars($loggedInUser->getPrenom(), ENT_QUOTES, 'UTF-8');
+                    $_SESSION['nom'] = htmlspecialchars($loggedInUser->getNom(), ENT_QUOTES, 'UTF-8');
+                    $_SESSION['email'] = htmlspecialchars($loggedInUser->getEmail(), ENT_QUOTES, 'UTF-8');
+                    $_SESSION['phone'] = htmlspecialchars($loggedInUser->getTelephone(), ENT_QUOTES, 'UTF-8');
+                    $_SESSION['role'] = htmlspecialchars($loggedInUser->getRole(), ENT_QUOTES, 'UTF-8');
 
-                    if($_SESSION['role'] ==='Admin'){
+                    if($_SESSION['role'] === 'Admin'){
                         header("Location: ../views/admin/dashboard.php");
-                    }else if($_SESSION['role'] ==='Auteur'){
+                    } else if($_SESSION['role'] === 'Auteur'){
                         header("Location: ../views/auteur/dashboard.php");
-                    }else if($_SESSION['role'] ==='Utilisateur'){
+                    } else if($_SESSION['role'] === 'Utilisateur'){
                         header("Location: ../views/user/articles.php");
                     }
                     exit;
@@ -37,7 +36,6 @@
                     echo "Identifiants incorrects.";
                 }
             }
-            
         }
     }
 ?>
