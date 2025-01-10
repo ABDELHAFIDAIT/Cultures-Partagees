d98be2dcd560025603507dac81ab053781628b61
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2025-01-10 10:09:02 +0100

    Afficher les tags pour toute les article dans Dash Admin

--- views/admin/article.php
+++ views/admin/article.php
@@ -24,11 +24,15 @@
 
                     <?php
                         require_once '../../classes/article.php';
+                        require_once '../../classes/tag.php';
                         $id = $_GET['id'];
                         $article = new Article();
                         $result = $article->getArticle($id);
 
+                        $tg = new Tag();
+
                         if($result){
+                            $tags = $tg->articleTag((int)$result['id_article']);
                             echo ' 
                             <div>
                                 <img src="../../uploads/'. $result['couverture'] .'" class="rounded-t-md w-full" alt="Couverture de l\'Article">
@@ -39,6 +43,17 @@
                             <p class="text-gray-800 font-medium text-lg">' . $result['date_publication'] . ' •</p>
                             <p class="text-gray-800 font-medium text-lg">Par : ' . $result['prenom'] . ' ' . $result['nom']. '</p>
                             </div>';
+                            echo '<div class="flex items-center gap-1 mt-2 mb-4 flex-wrap">';
+                            if(is_array($tags)){
+                                foreach ($tags as $tag) {
+                                    if(is_array($tag)) {
+                                        echo '<div class="text-xs text-white bg-blue-700 rounded-full py-1 px-3">#  '. $tag['nom_tag'] .'</div>';
+                                    }
+                                }
+                            }else{
+                                echo '<p class="text-xs font-extralight text-red-400"># Pas de Tags pour Cet Article !</p>';
+                            }
+                            echo '</div>';
                             echo '<h1 class="text-gray-900 font-semibold text-xl mb-3">'. $result['titre'] .'</h1>';
                             echo '<p class="text-gray-700 font-medium text-md text-justify">'. $result['contenu'] .'</p>';
                             echo '<p class="absolute top-5 left-5 bg-white bg-opacity-85 py-1 px-3 rounded-md text-xs">' . $result['nom_categorie'] . '</p>';
